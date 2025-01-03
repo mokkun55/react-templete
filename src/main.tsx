dbb0aa2d9eec018e0179c5bd0caf69563e0b09a8
もっくん <mokkunpc@gmail.com>
2025-01-03 14:04:38 +0900

    feat: alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BaseProvider } from './providers/baseProvider'
-import { Router } from './router/Route'
+import { BaseProvider } from '@/providers/baseProvider'
+import { Router } from '@/router/Route'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
